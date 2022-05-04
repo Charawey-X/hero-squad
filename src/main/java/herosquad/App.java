ecf3c4193cf127a7da606529a7fac63c2cc89bcc
Charawey-X <charawey@gmail.com>
2022-05-04 19:52:16 +0300

    added images

--- src/main/java/herosquad/App.java
+++ src/main/java/herosquad/App.java
@@ -1,7 +1,30 @@
 package herosquad;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
 import static spark.Spark.*;
 
 
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        port(4200);
+        get("/",(request, response) ->{
+            return new ModelAndView(new HashMap(), "index.hbs");
+
+        },new HandlebarsTemplateEngine());
+
+        get("/heroes",(request, response) ->{
+            return new ModelAndView(new HashMap(), "heroes.hbs");
+
+        },new HandlebarsTemplateEngine());
+
+        get("/squads",(request, response) ->{
+            return new ModelAndView(new HashMap(), "squads.hbs");
+
+        },new HandlebarsTemplateEngine());
+    }
 }
