49ccba1d071aaedd8b628b987884edc2e4bb3de1
Charawey-X <charawey@gmail.com>
2022-05-05 07:14:57 +0300

    edited form links

--- src/main/java/herosquad/App.java
+++ src/main/java/herosquad/App.java
@@ -3,7 +3,9 @@ package herosquad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -18,7 +20,12 @@ public class App {
         },new HandlebarsTemplateEngine());
 
         get("/heroes",(request, response) ->{
-            return new ModelAndView(new HashMap(), "heroes.hbs");
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getInstances();
+            model.put("hero", hero);
+            ArrayList<Squad> squads = Squad.getInstances();
+            model.put("squads", squads);
+            return new ModelAndView(model, "heroes.hbs");
 
         },new HandlebarsTemplateEngine());
 
@@ -26,5 +33,22 @@ public class App {
             return new ModelAndView(new HashMap(), "squads.hbs");
 
         },new HandlebarsTemplateEngine());
+
+        get("/form",(request, response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+
+        },new HandlebarsTemplateEngine());
+
+        post("/heroes", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String heroName = request.queryParams("name");
+            Integer heroAge = Integer.valueOf(request.queryParams("age"));
+            String heroPower = request.queryParams("superpower");
+            String heroWeakness = request.queryParams("weakness");
+            Hero hero = new Hero(heroName, heroAge, heroPower, heroWeakness);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
