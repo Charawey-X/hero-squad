b0e53f33e6fededd1fe8b5dcb360960eb3622ef7
Charawey-X <charawey@gmail.com>
2022-05-05 07:28:45 +0300

    edited form links

--- src/main/java/herosquad/App.java
+++ src/main/java/herosquad/App.java
@@ -35,18 +35,36 @@ public class App {
         },new HandlebarsTemplateEngine());
 
         get("/form",(request, response) ->{
-            Map<String, Object> model = new HashMap<String, Object>();
+            Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "form.hbs");
 
         },new HandlebarsTemplateEngine());
 
         post("/heroes", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
+            Map<String, Object> model = new HashMap<>();
             String heroName = request.queryParams("name");
             Integer heroAge = Integer.valueOf(request.queryParams("age"));
             String heroPower = request.queryParams("superpower");
             String heroWeakness = request.queryParams("weakness");
             Hero hero = new Hero(heroName, heroAge, heroPower, heroWeakness);
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/form_two", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getInstances();
+            model.put("hero", hero);
+            ArrayList<Squad> squads = Squad.getInstances();
+            model.put("squads", squads);
+            return new ModelAndView(model, "form two.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+        post("/squads", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String squadName = request.queryParams("squadName");
+            String squadCause = request.queryParams("squadCause");
+            Squad squad = new Squad(squadName, squadCause);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
--- src/main/java/herosquad/Squad.java
+++ src/main/java/herosquad/Squad.java
@@ -1,4 +1,49 @@
 package herosquad;
 
+import java.util.ArrayList;
+
 public class Squad {
+    private String squadName;
+
+    private String squadCause;
+
+    private static ArrayList<Squad> instances = new ArrayList<>();
+
+    public static void clearSquads(){
+        instances.clear();
+    }
+
+    private int id;
+
+    public Squad(String squadName, String squadCause){
+        this.squadName = squadName;
+        this.squadCause = squadCause;
+        instances.add(this);
+        this.id = instances.size();
+    }
+
+
+    public String getSquadName() {
+        return squadName;
+    }
+
+    public void setSquadName(String squadName) {
+        this.squadName = squadName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static ArrayList<Squad> getInstances() {
+        return instances;
+    }
+
+    public String getSquadCause() {
+        return squadCause;
+    }
+
+    public void setSquadCause(String squadCause) {
+        this.squadCause = squadCause;
+    }
 }
