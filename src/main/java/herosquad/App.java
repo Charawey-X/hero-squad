a3c38566fa587665a735e71a40c21e306e1ef893
Charawey-X <charawey@gmail.com>
2022-05-07 13:09:12 +0300

    Updated README

--- src/main/java/herosquad/App.java
+++ src/main/java/herosquad/App.java
@@ -11,9 +11,16 @@ import static spark.Spark.*;
 
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
-        port(4200);
         get("/",(request, response) ->{
             return new ModelAndView(new HashMap(), "index.hbs");
 
